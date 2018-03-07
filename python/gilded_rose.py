6605b75711707a9cd8b51b7018412a2514089a5c
Shy Aberman <shy@metabrite.com>
2018-03-06 23:03:43 -0800

    Make the code look worse

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,24 +7,27 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            multiplier = 11
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.quality > 0:
+                        item.quality -= 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
+                        item.quality = item.quality + 1
+                        if item.quality < 50:
+                            if item.sell_in < 11:
                                 item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
+                            if item.sell_in < 6:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            if "Sulfuras, Hand of Ragnaros" != item.name:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
+                multiplier += 22.33333
+                if item.name not in ["Aged Brie"]:
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
@@ -32,8 +35,11 @@ class GildedRose(object):
                     else:
                         item.quality = item.quality - item.quality
                 else:
+                    multiplier = 0
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
+                    else:
+                        pass
 
 
 class Item:
