c230d5c535e69bb09d4972b9c94c6d5d7fa5a7b2
Shy Aberman <shy@ripl.com>
2018-10-04 12:24:20 -0700

    More stuff

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -14,53 +14,54 @@ class GildedRose
     @items.each do |item|
 
       if is_legendary_item(item)
-
+        next
       end
 
-      if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES
-        if !is_legendary_item(item)
-          decrement_quality_of_standard_item(item)
-        end
+      item.sell_in = item.sell_in - 1
+
+      if gets_better_with_time(item)
+        decrement_quality(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == BACKSTAGE_PASSES
-            if item.quality < 50
-              if item.sell_in < 11
-                item.quality = item.quality + 1
-              end
-              if item.sell_in < 6
-                item.quality = item.quality + 1
-              end
-            end
+        increment_quality(item)
+        if is_backstage_pass(item)
+          if item.sell_in < 10
+            increment_quality(item)
+          end
+          if item.sell_in < 5
+            increment_quality(item)
           end
         end
       end
-      if LEGENDARY_ITEM_SULFURAS != item.name
-        item.sell_in = item.sell_in - 1
-      end
+
       if has_sell_in_expired(item)
-        if ![AGED_BRIE].include? item.name
+        if item.name != AGED_BRIE
           if item.name != BACKSTAGE_PASSES
-            if item.quality > 0
-              if !is_legendary_item(item)
-                item.quality = item.quality - 1
-              end
-            end
+            decrement_quality(item)
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          increment_quality(item)
         end
       end
-
     end
   end
 
   private
+
+  def is_backstage_pass(item)
+    item.name == BACKSTAGE_PASSES
+  end
+
+  def gets_better_with_time(item)
+    item.name != AGED_BRIE and !is_backstage_pass(item)
+  end
+
+  def increment_quality(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+    end
+  end
 
   def has_sell_in_expired(item)
     item.sell_in < 0
@@ -70,7 +71,7 @@ class GildedRose
     item.name == LEGENDARY_ITEM_SULFURAS
   end
 
-  def decrement_quality_of_standard_item(item)
+  def decrement_quality(item)
     if item.quality > 0
       item.quality = item.quality - 1
     end
