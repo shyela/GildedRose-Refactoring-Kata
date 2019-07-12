557e0d899019a9b5f6f746b057e989445bd2d836
Shy Aberman <shy@ripl.com>
2019-07-12 12:13:03 -0700

    Changes from July 12

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -5,57 +5,73 @@ class GildedRose
   end
 
   def update_quality()
-    @items.each do |item|
-      multiplier = 11
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            item.quality = item.quality + 1
-            if item.quality < 50
-              if item.sell_in < 11
-                item.quality = item.quality + 1
-              end
-              if item.sell_in < 6
-                item.quality = item.quality + 1
-              end
-            end
-            item.quality = item.quality - 1
-          end
-        end
+    @items.each(&method(:update_quality_for_item))
+  end
+
+  private
+
+  MAX_QUALITY = 50
+
+  def update_quality_for_item(item)
+    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if can_decrease_quality(item)
+        decrementQuality(item)
       end
-      if "Sulfuras, Hand of Ragnaros" != item.name
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        multiplier = multiplier + 22.33333
-        if !["Aged Brie"].include? item.name
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+    else
+      if can_increase_quality(item)
+        incrementQuality(item)
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if can_increase_quality(item)
+            if item.sell_in < 11
+              incrementQuality(item)
             end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          else
-            nil
+            if item.sell_in < 6
+              incrementQuality(item)
+            end
           end
         end
       end
     end
+    if "Sulfuras, Hand of Ragnaros" != item.name
+      item.sell_in = item.sell_in - 1
+    end
+    if item.sell_in < 0
+      if !["Aged Brie"].include? item.name
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if can_decrease_quality(item)
+            decrementQuality(item)
+          end
+        else
+          item.quality = item.quality - item.quality
+        end
+      else
+        if can_increase_quality(item)
+          incrementQuality(item)
+        else
+          nil
+        end
+      end
+    end
+  end
+
+  def can_decrease_quality(item)
+    item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros"
+  end
+
+  def can_increase_quality(item)
+    item.quality < MAX_QUALITY
+  end
+
+  def incrementQuality(item)
+    item.quality = item.quality + 1
+  end
+
+  def decrementQuality(item)
+    item.quality = item.quality - 1
   end
 end
+
+
 
 # DO NOT MODIFY THIS CLASS
 class Item
