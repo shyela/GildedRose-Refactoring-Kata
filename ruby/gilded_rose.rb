81d090cf6264066588b4614f7765c914975825a2
Shy Aberman <shy@ripl.com>
2019-08-02 12:05:25 -0700

    Mob changes

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -13,65 +13,75 @@ class GildedRose
   MAX_QUALITY = 50
 
   def update_quality_for_item(item)
-    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-      if can_decrease_quality(item)
-        decrementQuality(item)
-      end
+    if is_not_aged_item?(item) && is_not_backstage_pass?(item)
+      decrement_quality(item)
     else
-      if can_increase_quality(item)
-        incrementQuality(item)
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if can_increase_quality(item)
-            if item.sell_in < 11
-              incrementQuality(item)
-            end
-            if item.sell_in < 6
-              incrementQuality(item)
-            end
-          end
-        end
-      end
+      increment_quality(item)
+      additionally_increase_backstage_pass_quality(item)
     end
-    if "Sulfuras, Hand of Ragnaros" != item.name
+    if is_not_legendary_item?(item)
       item.sell_in = item.sell_in - 1
     end
     if item.sell_in < 0
-      if !["Aged Brie"].include? item.name
-        if item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if can_decrease_quality(item)
-            decrementQuality(item)
-          end
+      if is_not_aged_item?(item)
+        if is_not_backstage_pass?(item)
+          decrement_quality(item)
         else
-          item.quality = item.quality - item.quality
+          item.quality = 0
         end
       else
-        if can_increase_quality(item)
-          incrementQuality(item)
-        else
-          nil
-        end
+        increment_quality(item)
       end
     end
   end
 
+  def additionally_increase_backstage_pass_quality(item)
+    if is_backstage_pass?(item)
+      if item.sell_in < 11
+        increment_quality(item)
+      end
+      if item.sell_in < 6
+        increment_quality(item)
+      end
+    end
+  end
+
+  def is_not_aged_item?(item)
+    item.name != "Aged Brie"
+  end
+
+  def is_backstage_pass?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def is_not_backstage_pass?(item)
+    !is_backstage_pass?(item)
+  end
+
   def can_decrease_quality(item)
-    item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros"
+    item.quality > 0 && is_not_legendary_item?(item)
+  end
+
+  def is_not_legendary_item?(item)
+    item.name != "Sulfuras, Hand of Ragnaros"
   end
 
   def can_increase_quality(item)
     item.quality < MAX_QUALITY
   end
 
-  def incrementQuality(item)
-    item.quality = item.quality + 1
+  def increment_quality(item)
+    if can_increase_quality(item)
+      item.quality = item.quality + 1
+    end
   end
 
-  def decrementQuality(item)
-    item.quality = item.quality - 1
+  def decrement_quality(item)
+    if can_decrease_quality(item)
+      item.quality = item.quality - 1
+    end
   end
 end
-
-
 
 # DO NOT MODIFY THIS CLASS
 class Item
