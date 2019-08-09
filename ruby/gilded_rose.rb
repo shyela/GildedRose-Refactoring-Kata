165c2d4c384a7c7ec7a2ce40e579dbc3fa2b4fbc
James Orrell <james@ripl.com>
2019-08-09 12:02:54 -0700

    refactoring practice on 08-09-19

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -8,62 +8,75 @@ class GildedRose
     @items.each(&method(:update_quality_for_item))
   end
 
-  private
-
   MAX_QUALITY = 50
 
   def update_quality_for_item(item)
-    if is_not_aged_item?(item) && is_not_backstage_pass?(item)
-      decrement_quality(item)
-    else
-      increment_quality(item)
-      additionally_increase_backstage_pass_quality(item)
+    if is_legendary_item?(item)
+      return
     end
-    if is_not_legendary_item?(item)
-      item.sell_in = item.sell_in - 1
-    end
-    if item.sell_in < 0
-      if is_not_aged_item?(item)
-        if is_not_backstage_pass?(item)
-          decrement_quality(item)
-        else
-          item.quality = 0
-        end
-      else
-        increment_quality(item)
-      end
-    end
-  end
 
-  def additionally_increase_backstage_pass_quality(item)
+    if is_aged_item?(item)
+      handle_aged_item(item)
+      return
+    end
+
     if is_backstage_pass?(item)
-      if item.sell_in < 11
-        increment_quality(item)
-      end
-      if item.sell_in < 6
-        increment_quality(item)
-      end
+      handle_backstage_pass(item)
+      return
+    end
+
+    handle_generic_item(item)
+  end
+
+  def handle_aged_item(item)
+    increment_quality(item)
+    decrement_sell_in(item)
+    if item.sell_in < 0
+      increment_quality(item)
     end
   end
 
-  def is_not_aged_item?(item)
-    item.name != "Aged Brie"
+  def handle_backstage_pass(item)
+    increase_backstage_pass_quality(item)
+    decrement_sell_in(item)
+    if item.sell_in < 0
+      expire_item(item)
+    end
+  end
+
+  def handle_generic_item(item)
+    decrement_quality(item)
+    decrement_sell_in(item)
+
+    if item.sell_in < 0
+      decrement_quality(item)
+    end
+  end
+
+  def is_aged_item?(item)
+    item.name == "Aged Brie"
+  end
+
+  def is_legendary_item?(item)
+    item.name == "Sulfuras, Hand of Ragnaros"
   end
 
   def is_backstage_pass?(item)
     item.name == "Backstage passes to a TAFKAL80ETC concert"
   end
 
-  def is_not_backstage_pass?(item)
-    !is_backstage_pass?(item)
+  def increase_backstage_pass_quality(item)
+    increment_quality(item)
+    if item.sell_in < 11
+      increment_quality(item)
+    end
+    if item.sell_in < 6
+      increment_quality(item)
+    end
   end
 
   def can_decrease_quality(item)
-    item.quality > 0 && is_not_legendary_item?(item)
-  end
-
-  def is_not_legendary_item?(item)
-    item.name != "Sulfuras, Hand of Ragnaros"
+    item.quality > 0
   end
 
   def can_increase_quality(item)
@@ -80,6 +93,14 @@ class GildedRose
     if can_decrease_quality(item)
       item.quality = item.quality - 1
     end
+  end
+
+  def decrement_sell_in(item)
+    item.sell_in = item.sell_in - 1
+  end
+
+  def expire_item(item)
+    item.quality = 0
   end
 end
 
